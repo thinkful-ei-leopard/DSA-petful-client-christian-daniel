4c64fc499c29160be99c9e608051621310580653
shimmy77 <christianshim@gmail.com>
2020-04-10 10:54:41 -0500

    almost with user story 1

--- petful-client/src/components/home.js
+++ petful-client/src/components/home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import dog_cat from '../images/dog_cat.png'
+import './home.css'
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -9,9 +11,16 @@ export default class Home extends React.Component {
     }
     render() {
         return (
-            <div>
-                Welcome to Petful!
-            </div>
+            <heading>
+                <h1>Welcome to Petful!</h1>
+                <p classname="intro">This is an online animal shelter which allows you to adopt cats and dogs. <br></br>
+                    This is strictly based on "First in, First out". You can adopt a cat, a dog <br></br>
+                    or both, but you may only adopt the animal that came to the shelter first.<br></br>
+                    You will be put into queues so you must wait for your turn.
+                </p>
+                <img src={dog_cat} />
+            </heading>
+
         )
     }
 }
